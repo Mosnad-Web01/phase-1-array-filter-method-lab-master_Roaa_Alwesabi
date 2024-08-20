117df0affebe4b0ff2ff71b8988ea444af49ef9c
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-08-20 13:21:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
